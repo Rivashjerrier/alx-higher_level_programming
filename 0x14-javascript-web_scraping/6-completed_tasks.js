79934ab3714cfde719d6864ba9e516973d51acd2
Rivashjerrier <rivijerrier@gmail.com>
2023-01-17 23:12:04 +0000

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,25 @@
 #!/usr/bin/node
 
+const request = require('request');
 
+request(process.argv[2], (err, response, body) => {
+  if (!err) {
+    const tasks = JSON.parse(body);
+    const completed_tasks = [];
+    tasks.forEach(function (user) {
+      if (completed_tasks[user.userId] === undefined) {
+        completed_tasks[user.userId] = 0;
+      }
+      if (user.completed === true) {
+        completed_tasks[user.userId]++;
+      }
+   });
+    const number = {};
+   for (let i = 1; i < completed_tasks.length; ++i) {
+      if (complete_tasks[i] > 0) {
+        number[i] = completed_tasks[i];
+      }
+    }
+    console.log(number);
+  }
+});
