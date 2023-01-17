fdc45dd2cdf3daff6801d59a2aa2632f7196e0c6
Rivashjerrier <rivijerrier@gmail.com>
2023-01-17 12:46:25 +0000

    display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], (error, response, body) => {
+
+request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
