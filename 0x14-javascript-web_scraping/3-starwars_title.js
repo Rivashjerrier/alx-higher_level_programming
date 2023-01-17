1f0a19ac50ff8571419f5a7becd0f0b543ac6856
Rivashjerrier <rivijerrier@gmail.com>
2023-01-17 12:50:10 +0000

    prints the title of a Star Wars movie where the episode number matches a given integer

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,12 @@
 
 const request = require('request');
 const API_url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-const data = JSON.parse(body);
 
 request(API_url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
+    const info = JSON.parse(body);
     console.log(data.title);
   }
 });
