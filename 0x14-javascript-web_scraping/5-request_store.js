e7cf5f85776598a52cebfdb46829a28c138fc533
Rivashjerrier <rivijerrier@gmail.com>
2023-01-17 13:16:51 +0000

    gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const request = require('request');
 
-request(process.argv[2], (error, response, body) => {
+request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
