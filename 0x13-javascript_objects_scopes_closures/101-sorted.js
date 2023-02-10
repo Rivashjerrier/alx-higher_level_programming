75a3c2da69a898c47cc3d487712686cd016c8a7b
Rivashjerrier <rivijerrier@gmail.com>
2023-02-10 20:55:10 +0000

    imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-const new_dict = {};
+const newDict = {};
 
 for (const key in dict) {
-  if (dict[key] in new_dict) {
-    new_dict[dict[key]].push(key);
+  if (dict[key] in newDict) {
+    newDict[dict[key]].push(key);
   } else {
-    new_dict[dict[key]] = [key];
+    newDict[dict[key]] = [key];
   }
 }
 
