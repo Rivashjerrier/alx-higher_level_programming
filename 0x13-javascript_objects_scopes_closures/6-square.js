669c8ca22279c11bc70f7e23f9ea4d0ec917661c
Rivashjerrier <rivijerrier@gmail.com>
2023-02-10 18:04:00 +0000

    prints the rectangle using the character c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Square#0 = require('./5-square');
+
+class Square#1 extends Square#0 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
