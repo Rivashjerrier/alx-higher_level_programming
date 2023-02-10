667596d29b6b68091ce547611888f2043e116ba0
Rivashjerrier <rivijerrier@gmail.com>
2023-02-10 19:42:19 +0000

    returns the number of occurrences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let num = 0;
   for (let x = 0; x < list.length; ++x) {
-    if (list[i] === searchElement) {
+    if (list[x] === searchElement) {
       num++;
     }
   }
