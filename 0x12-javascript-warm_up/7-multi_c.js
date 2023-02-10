b75f58b5557d1d94e548470bc6f6d42c8966c0ec
Rivashjerrier <rivijerrier@gmail.com>
2023-02-10 12:05:09 +0000

    prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ if (isNaN(parseInt(int))) {
   console.log('Missing number of occurrences');
 } else {
   if (int > 0) {
-    for (let x = 1; x <= arg; ++x) {
+    for (let x = 1; x <= int; ++x) {
       console.log('C is fun');
     }
   }
