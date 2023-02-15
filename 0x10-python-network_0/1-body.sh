163701d3cd07775b06966ee63ac9342cdae245c7
Rivashjerrier <rivijerrier@gmail.com>
2023-02-15 23:20:03 +0000

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL using curl, and displays the body of the response (if the status code is 200)
-curl -s $1 | awk 'BEGIN { RS="\r\n\r\n" } { if (NR == 1) { split($0, status, " "); if (status[2] == 200) { print $0 } } else { print $0 } }'
-
+# This script takes in a URL, sends a GET request to the URL using curl, and displays the body of the response
+curl -sfL "$1" -X GET
