6ebb52049dfac416f5cfbe52bd4d8a7cbd2ea4e4
Rivashjerrier <rivijerrier@gmail.com>
2023-02-15 17:56:05 +0000

    adds the State object Louisiana to the db

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     session = Session()
     add_state = State(name="Louisiana")
     session.add(add_state)
-    print(add_state.id)
     session.commit()
+    print(add_state.id)
     session.close()
